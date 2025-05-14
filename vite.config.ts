f3427dc4acaf95a52851299fcc72c8ce94cdfd34
fengruoshi <fengrs@1token.trade>
2025-05-14 11:11:17 +0800

    u

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/https://github.com/frace-feng/moment-demo/',
+  base: '/moment-demo/dist',
   plugins: [vue()],
 })
